77538dfc6ba8b13a9b70e6a4b1bf60c04f0a74f7
MGKineesh <mattken02@gmail.com>
2022-05-18 23:02:44 +1000

    started final puzzle

---
+++ Point_&_Click/Assets/Scripts/Combination Puzzle/MoveIngredient.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIngredient : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] GameObject ingredient;
+    private float cameraZDistance;
+    private const string potTag = "Pot";
+    private int colorChangeAmount = 85;
+    private Renderer potionMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject potion = GameObject.Find("Pot");
+        cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        potionMaterial = potion.GetComponent<Renderer>();
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 ScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+        Vector3 NewWorldPosition = mainCamera.ScreenToWorldPoint(ScreenPosition);
+        transform.position = NewWorldPosition;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(potTag))
+        {
+            ingredient.SetActive(false);
+            Debug.Log("Object Entered");
+            //potionMaterial.material.color
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
