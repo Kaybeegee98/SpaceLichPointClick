d5fbe89fa6feceb3dd776ae0707355104c358c1e
Shoica <jessicasholakis@gmail.com>
2022-05-24 12:10:48 +1000

    menu code updated

--- Point_&_Click/Assets/Scripts/MenuScript.cs
+++ Point_&_Click/Assets/Scripts/MenuScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-//     private InputField inputtedCode;
+    private InputField inputtedCode;
 
-//     void Start() {
-//         inputtedCode = GetComponent<InputField>();
-//     }
+    void Start() {
+        inputtedCode = GetComponent<InputField>();
+    }
 
-//     public void CheckCodeAndLoad() {
-//         if (inputtedCode == "idk") {
-//             SceneManager.LoadScene("Skip_To_Room_Two");
-//         }
-//     }
+    public void CheckCodeAndLoad() {
+        if (inputtedCode.text.Trim().ToLower() == "phantom") {
+            SceneManager.LoadScene("Skip_To_Room_Two");
+        }
+    }
 }
