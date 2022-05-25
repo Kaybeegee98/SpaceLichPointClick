68945d8a19a8a4cfa6ed60c59d07aa5b545ad469
Kaybeegee98 <102107602@student.swin.edu.au>
2022-05-25 20:56:38 +1000

    Resume Room Done

--- Point_&_Click/Assets/Scripts/ResumeRoom.cs
+++ Point_&_Click/Assets/Scripts/ResumeRoom.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class ResumeRoom : MonoBehaviour
 {
+    //Lever Puzzle
     public LeversManager levManager;
+
+    //Fishing Puzzle
     public fishing_minigame fishMini;
 
+    //Roof Puzzle
     public RotateRing ringInner;
     public RotateRing ringMiddle;
     public RotateRing ringOuter;
+
+    //Matching Puzzle
+    public MatchSystemManager matchManager;
+    public List<Transform> MoveablePairs;
+    public List<Transform> Ports;
+
+    //Turn on Door Camera
+    public BoxPuzzle boxPuzzle;
 
     [HideInInspector]
     private bool phase1 = false;
@@ -52,5 +64,22 @@ public class ResumeRoom : MonoBehaviour
         Destroy(ringMiddle.GetComponent<Collider>());
         Destroy(ringOuter.GetComponent<Collider>());
         Destroy(ringInner.GetComponent<Collider>());
+
+
+        //Matching Puzzle
+        matchManager.orb = null;
+        matchManager.click = null;
+
+        for (int i = 0; i < 4; i++)
+        {
+            MoveablePairs[i].transform.position = Ports[i].transform.position;
+        }
+
+        //Door Camera On
+        if (boxPuzzle.activated != true)
+        {
+            boxPuzzle.activated = true;
+            boxPuzzle.GetComponent<Location>().Arrive();
+        }
     }
 }
