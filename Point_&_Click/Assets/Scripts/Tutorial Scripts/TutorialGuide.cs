a23b790fa4e7c883bea81d0d8b7ec977d3b674c7
Shoica <jessicasholakis@gmail.com>
2022-05-25 21:51:51 +1000

    tutorial dialogue fits

--- Point_&_Click/Assets/Scripts/Tutorial Scripts/TutorialGuide.cs
+++ Point_&_Click/Assets/Scripts/Tutorial Scripts/TutorialGuide.cs
@@ -24,20 +24,20 @@ public class TutorialGuide : MonoBehaviour
             if (GameManager.ins.itemsHeld.Count > 0)
             {
                 orbCollected = true;
-                tutorialText.text = "Turn to face the pedestal, then Left Click to place the orb down";
+                tutorialText.text = "Turn to face the pedestal, then Left Click to place the orb down.";
             }
             else
             {
-                tutorialText.text = "Left Click on either side of the screen to turn around and view the room";
+                tutorialText.text = "Left Click on either side of the screen to turn around and view the room.";
             }
         }
         else if (GameManager.ins.currentNode.GetComponent<Prop>() != null)
         {
-            tutorialText.text = "Left Click on the Orb to pick it up and add it to your inventory. Then, Right Click to back out to the previous location";
+            tutorialText.text = "Left Click on the Orb to pick it up and add it to your inventory. Then, Right Click to back out to the previous location.";
         }
         else if (GameManager.ins.currentNode.GetComponent<Location>() != null && roomVisted == true)
         {
-            tutorialText.text = "Left Click on an object to inspect it, or Right Click to go back to the room";
+            tutorialText.text = "Left Click on an object to inspect it, or Right Click to go back to the room.";
         }
     }
 }
